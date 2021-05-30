d65133e794c7c0a5dc4c87831a32b67b863ed428
Dan Kent <patorn.5@mail.kmutt.ac.th>
2021-05-30 16:43:04 +0700

    [UPDATE] bring back the previous line

--- leash-express/routes/feedRoute.js
+++ leash-express/routes/feedRoute.js
@@ -8,6 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors())
+app.use(express.static('public'));
 
 //Post Models
 const PostModel = require('../models/Post');
